192124e6f2f9741974e70161a8682975b659cee8
Skrubstar <wangzilongcharles@gmail.com>
2024-02-17 14:37:40 -0800

    Test One of adding and Loading the CSV

---
+++ src/components/App.svelte
@@ -0,0 +1,27 @@
+<script>
+    import * as d3 from 'd3';
+
+    let energyData = [];
+
+    onMount(async () => {
+
+     const res = await fetch('owid-energy-data.csv'); 
+
+     const csv = await res.text();
+
+     energyData = d3.csvParse(csv, d3.autoType)
+
+     console.log(energyData);
+
+ });
+</script>
+
+<main>
+    <!-- Add HTML elements here -->
+</main>
+
+<style>
+    main {
+        // Add CSS styling here, scoped to only work on App.svelte
+    }
+</style>
