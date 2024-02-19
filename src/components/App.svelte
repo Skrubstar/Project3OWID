30f0c5435b82f32e52029c9fd1d67b3485cb83a9
Skrubstar <wangzilongcharles@gmail.com>
2024-02-18 19:51:51 -0800

    Added map visualization

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -15,15 +15,42 @@
      console.log(energyData);
 
  });
+    let width = 1920;
+    let height = 1080;
+    var svg = d3.select("body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+    var projection = d3.geoMercator()
+        .scale(175)
+        .center([-11, 85]);
+    var path = d3.geoPath().projection(projection);
+
+    let dataset = [];
+    d3.json("geojson.json"
+    ).then((data)  => {
+    dataset  =  data.features;
+    });
+
+
 </script>
 
 <main>
     <h1>Hello Team!</h1>
     <p>Svelte Testing.</p>
+    <svg width={height} height={height}>
+        {#each  dataset  as data}
+            <path  d={path(data)} />
+        {/each}
+    </svg>
 </main>
 
 <style>
-    main {
-        text-align: center;
+    * {
+        text-align:center;
+    }
+    path {
+        fill: none;
+        stroke: darkgreen;
     }
 </style>
