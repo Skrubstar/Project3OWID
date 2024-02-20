5d29ba37525afb8b1d05575f1760b63b3518809b
Skrubstar <wangzilongcharles@gmail.com>
2024-02-19 23:32:35 -0800

    Linked Data CSV to GeoJSON, added index property so can assign color values

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -4,46 +4,50 @@
 
     let energyData = [];
 
-    onMount(async () => {
-
-     const res = await fetch('owid-energy-data.csv'); 
-
-     const csv = await res.text();
-
-     energyData = d3.csvParse(csv, d3.autoType)
-
-     console.log(energyData);
-
- });
-    let width = 1920;
-    let height = 1080;
-    var svg = d3.select("body")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
-    var projection = d3.geoMercator()
-        .scale(175)
-        .center([-11, 85]);
-    var path = d3.geoPath().projection(projection);
-
     let dataset = [];
-    d3.json("geojson.json"
-    ).then((data)  => {
-    dataset  =  data.features;
-    });
+    let width = 1280;
+    let height = 720;
+        Promise.all([d3.csv("recent_energy_data.csv"),
+        d3.json('geojson.json')])
+            .then(([energyData, dataset]) => {
+                for (var i = 0; i < energyData.length; i++) {
+                    var iso_country_code = energyData[i].iso_country;
+                    var index = energyData[i].index;
+                    for (var j = 0; j < dataset.features.length; j++) {
+                        var country_code_json = dataset.features[j].properties.iso_a3
+                        if (country_code_json == iso_country_code) {
+                            dataset.features[j].properties.corresponding_index = index;
+                            break;
+                            }
+                        }
+                    }
+                console.log(dataset)
+                var countries = g.selectAll("path")
+                    .data(dataset.features)
+                    .enter().append("path")
+                    .attr("d", path)
+                    .attr("fill", "lightgray")
+                    .attr("stroke", "white");
+            });
 
+        let svg = d3.select("body")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .style("display", "block")
+            .style("margin", "auto")
+        var g = svg.append('g');
+        var projection = d3.geoNaturalEarth1()
+            .scale(250)
+            .center([-30, 35])
+        var path = d3.geoPath().projection(projection);
 
 </script>
 
 <main>
     <h1>Hello Team!</h1>
     <p>Svelte Testing.</p>
-    <svg width={height} height={height}>
-        {#each  dataset  as data}
-            <path  d={path(data)} />
-        {/each}
-    </svg>
-</main>
+    </main>
 
 <style>
     * {
